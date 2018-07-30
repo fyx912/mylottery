3e2b56c31af3774c18ecd47d9a31d6e6f3ef8054
dingyaqiang <12345678>
2018-07-30 21:17:38 +0800

    add miles data sources

--- api/src/main/java/com/yys/lottery/api/ApiMain.java
+++ api/src/main/java/com/yys/lottery/api/ApiMain.java
@@ -1,9 +1,9 @@
 package com.yys.lottery.api;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -13,9 +13,8 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableCaching
 @ServletComponentScan
 @EnableAutoConfiguration
-@MapperScan(basePackages = {"com.yys.lottery.*.dao.*"})
 @ComponentScan(basePackages = {"com.yys.*"})
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ApiMain extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
